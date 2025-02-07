ed841d033726e2fe790fe54d8f43b7176f617ec2
George Durden <george@durden.com>
2025-02-07 12:50:43 -0500

    Updated Comments in Code

--- ESP32-TFT-DEMO.ino
+++ ESP32-TFT-DEMO.ino
@@ -75,22 +75,30 @@ void setup() {
   Serial.begin(115200);
 
   // Start the SPI for the touchscreen and init the touchscreen
+  //
   touchscreenSPI.begin(XPT2046_CLK, XPT2046_MISO, XPT2046_MOSI, XPT2046_CS);
   touchscreen.begin(touchscreenSPI);
+
   // Set the Touchscreen rotation in landscape mode
   // Note: in some displays, the touchscreen might be upside down, so you might need to set the rotation to 3: touchscreen.setRotation(3);
+  //
   touchscreen.setRotation(1);
 
   // Start the tft display
+  //
   tft.init();
+
   // Set the TFT display rotation in landscape mode
+  //
   tft.setRotation(1);
 
   // Clear the screen before writing to it
+  //
   tft.fillScreen(TFT_WHITE);
   tft.setTextColor(TFT_BLACK, TFT_WHITE);
   
   // Set X and Y coordinates for center of display
+  //
   int centerX = SCREEN_WIDTH / 2;
   int centerY = SCREEN_HEIGHT / 2;
 
